95633c8af177b97bc405944e96ae92b043a61621
Wolfgang Schreurs <wolfgang.schreurs@gmail.com>
2024-03-27 23:16:53 +0700

    Improve mouse control

--- src/input_modes/mouse.lua
+++ src/input_modes/mouse.lua
@@ -1,5 +1,18 @@
 local Mouse = {}
 
+local function tryGetEnemy(level, coord)
+    local entities = level:getEntities(coord, function(e) return e.type == 'npc' end)
+    if #entities > 0 then
+        local target = entities[1]
+        local health = target:getComponent(Health)
+        if health:isAlive() then
+            return target
+        end
+    end
+
+    return nil
+end
+
 Mouse.new = function(entity)
     local getAction = function(self, level)
         if not love.mouse.isDown(1) then return end
@@ -13,21 +26,36 @@ Mouse.new = function(entity)
         end
 
         if level:isBlocked(next_coord) then
-            local entities = level:getEntities(next_coord, function(e) return e.type == 'npc' end)
-            if #entities > 0 then
-                local target = entities[1]
-                local health = target:getComponent(Health)
-                if health:isAlive() then
-                    return Attack(level, entity, target)
-                end
+            local target = tryGetEnemy(level, next_coord)
+            if target ~= nil then
+                return Attack(level, entity, target)
             end
 
             return nil
         end 
 
-        if not Direction.isOrdinal(direction) then
-            -- TODO: should allow movement when a diagonal direction is set
+        if Direction.isOrdinal(direction) then
+            local dxy = entity.coord - Pointer.getCoord()
+
+            if direction == Direction.NW then
+                direction = dxy.x < dxy.y and Direction.W or Direction.N 
+            elseif direction == Direction.SW then
+                direction = dxy.x < dxy.y and Direction.W or Direction.S 
+            elseif direction == Direction.NE then
+                direction = dxy.x < dxy.y and Direction.E or Direction.N 
+            elseif direction == Direction.SE then
+                direction = dxy.x < dxy.y and Direction.E or Direction.S 
+            end
+
+            next_coord = entity.coord + direction
+            if not level:isBlocked(next_coord) then
+                return Move(level, entity, next_coord)
+            end
+        else
             return Move(level, entity, next_coord)
+        end
+
+        if not Direction.isOrdinal(direction) then
         end
 
         return nil
@@ -35,7 +63,7 @@ Mouse.new = function(entity)
 
     return setmetatable({
         -- methods
-        getAction   = getAction,
+        getAction = getAction,
     }, Mouse)
 end
 
