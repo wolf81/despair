ce4a22984936108107a45747ed4c4862c2b1c539
Wolfgang Schreurs <wolfgang.schreurs@gmail.com>
2024-04-10 18:42:55 +0700

    Cod clean-up

--- src/scenes/game.lua
+++ src/scenes/game.lua
@@ -20,8 +20,6 @@ Game.new = function()
 
     local is_paused, show_inventory = false, false
 
-    local inventory = Inventory(player)
-
     local portrait = Portrait()
 
     local overlay = Overlay()
@@ -41,8 +39,6 @@ Game.new = function()
     local player_info_w = INFO_PANEL_WIDTH
 
     local draw = function(self) 
-        love.graphics.push()
-
         dungeon:draw(0, 0, WINDOW_W - player_info_w, WINDOW_H) 
 
         love.graphics.setColor(0.1, 0.1, 0.1, 1.0)
@@ -50,43 +46,12 @@ Game.new = function()
 
         player_info:draw(WINDOW_W - player_info_w, 0, player_info_w, WINDOW_H)
 
-        if show_inventory then
-            local inv_w, inv_h = inventory:getSize()
-            local inv_x = mfloor((WINDOW_W - player_info_w - inv_w) / 2)
-            local inv_y = mfloor((WINDOW_H - inv_h) / 2)
-            inventory:draw(inv_x, inv_y, inv_w, inv_h)
-        end
-
-        if is_paused then
-            love.graphics.setFont(FONT)
-
-            local text_w = FONT:getWidth('PAUSED')
-            local text_x = WINDOW_W - text_w - 25
-            local text_y = WINDOW_H - 30
-            local text_h = FONT:getHeight()
-
-            love.graphics.setColor(0.0, 0.0, 0.0, 0.7)
-            love.graphics.rectangle('fill', text_x - 5, text_y - 5, text_w + 10, text_h + 5 * 2)
- 
-            love.graphics.setLineWidth(1.0)
-            love.graphics.setColor(1.0, 1.0, 1.0, 1.0)
-            love.graphics.rectangle('line', text_x - 5.5, text_y - 5.5, text_w + 10, text_h + 5 * 2)
-
-            love.graphics.printf('PAUSED', text_x, text_y, text_w, 'left')
-        end
-
-        love.graphics.pop()
-
         overlay:draw()
     end
 
-    local inv = Inventory(player)
-
-    local keyPressed = function(self, key, scancode)
-        if key == 'space' then is_paused = (not is_paused) end
-        
+    local keyPressed = function(self, key, scancode)        
         if key == 'i' then
-            Gamestate.push(inv)
+            Gamestate.push(Inventory(player))
         end
     end
 
