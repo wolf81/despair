0e3b16ce7b83ee88c3414bb6de6579e067def674
Wolfgang Schreurs <wolfgang.schreurs@gmail.com>
2024-04-13 12:25:12 +0700

    Generate class-based portraits

--- src/hud/portrait.lua
+++ src/hud/portrait.lua
@@ -9,6 +9,27 @@ local lrandom = love.math.random
 
 local Portrait = {}
 
+local FACE_INDICES = { 47, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 65, 65, }
+
+local HAIR_INDICES = { 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 
+    46, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, }
+
+local HELM_INDICES = {
+    ['fighter'] = { 66, },
+    ['cleric']  = { 70, },
+    ['rogue']   = { 67, 68, 69, },
+    ['mage']    = { 71, 72, }
+}
+local ARMOR_INDICES = {
+    ['fighter'] = { 1, 2, 3, 4, },
+    ['cleric']  = { 14, 15, 16, },
+    ['rogue']   = { 125, 126, 127, },
+    ['mage']    = { 17, 18, 116, 117, 118 }
+}
+local ACCESSORY_INDICES = { 8, 9, 10, 11, 12, 13, 19, 20, 21, 22, 23, 24, 25, 26, }
+
+local BEARD_INDICES = { 76, 77, 78, 79, 80, 87, 88, 89, 90, 105, 106, 107, 108, 109, 110 }
+
 Portrait.new = function(player)
     local key = 'uf_portraits'
     local texture = TextureCache:get(key)
@@ -16,11 +37,26 @@ Portrait.new = function(player)
 
     -- TODO: adjust clothes, accessories for player race and/or class
 
-    local hair_idx = lrandom(27, 46)
     local face_idx = lrandom(51, 65)
-    local hat_idx = lrandom(66, 72)
-    local clothes_idx = lrandom(14, 18)
-    local beard_idx = lrandom(76, 80)
+
+    local hair_idx, helm_idx = 200, 200
+
+    if lrandom(1, 3) > 1 then
+        local helm_indices = HELM_INDICES[player.class]
+        helm_idx = helm_indices[lrandom(#helm_indices)]
+    elseif lrandom(1, 2) == 1 then
+        hair_idx = HAIR_INDICES[lrandom(#HAIR_INDICES)]
+    end
+
+    local armor_indices = ARMOR_INDICES[player.class]
+    local armor_idx = armor_indices[lrandom(#armor_indices)]
+
+    local beard_idx = 200
+    if lrandom(1, 4) > 1 then
+        beard_idx = BEARD_INDICES[lrandom(#BEARD_INDICES)]
+    end
+
+    -- TODO: add accesories
 
     local _, _, quad_w, quad_h = quads[1]:getViewport()
 
@@ -32,7 +68,7 @@ Portrait.new = function(player)
         love.graphics.draw(texture, quads[clothes_idx])
         love.graphics.draw(texture, quads[hair_idx])
         love.graphics.draw(texture, quads[beard_idx])
-        love.graphics.draw(texture, quads[hat_idx])
+        love.graphics.draw(texture, quads[helm_idx])
         love.graphics.draw(texture, quads[7])
     end)
 
