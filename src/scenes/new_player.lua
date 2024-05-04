c7bbba0f5108b0a2694f14b45fdcc24c2c7d055b
Wolfgang Schreurs <wolfgang.schreurs@gmail.com>
2024-05-04 08:35:27 +0700

    Roll stats for new player

--- src/scenes/new_player.lua
+++ src/scenes/new_player.lua
@@ -6,6 +6,30 @@ local function generateButtonTexture(title)
     return TextureGenerator.generateButtonTexture(120, 48, title)
 end
 
+local function getStatValues()
+    -- TODO: add race bonus
+
+    local stats = {}
+    for i = 1, 3 do
+        -- roll 1d6, store 4 values
+        local values = {}
+        for i = 1, 4 do
+            table.insert(values, ndn.dice('1d6').roll())
+        end
+        
+        -- sort values descending
+        table.sort(values, function(a, b) return a > b end)
+
+        -- use the 3 highest values as stat value
+        local value = 0
+        for j = 1, 3 do value = value + values[j] end
+        
+        table.insert(stats, value)
+    end
+    
+    return stats
+end
+
 NewPlayer.new = function()
     local image = TextureGenerator.generatePanelTexture(120, 48)
 
@@ -32,13 +56,14 @@ NewPlayer.new = function()
             'Fighter', 'Mage', 'Cleric', 'Rogue'))
     end
 
+    local stats = getStatValues()
     local function onSelectStats()
         Gamestate.push(AssignPoints(
             'ASSIGN STATS',
             {
-                { key = 'Strength',         value = 12 },
-                { key = 'Dexterity',        value = 13 },
-                { key = 'Mind',             value = 14 },
+                { key = 'Strength',         value = stats[1] },
+                { key = 'Dexterity',        value = stats[2] },
+                { key = 'Mind',             value = stats[3] },
             }, 
             3))
     end
