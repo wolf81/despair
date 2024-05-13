b982670bcd0e52149fa39c57365f339813f315f2
Wolf <wolf@ironsoftware.com>
2024-05-13 07:52:17 +0700

    Code clean-up

--- src/generators/texture_gen.lua
+++ src/generators/texture_gen.lua
@@ -67,22 +67,22 @@ end
 M.generateContainerTexture = function() 
     local texture = TextureCache:get('uf_interface')
     local quads = QuadCache:get('uf_interface')
-    local color_info = ColorHelper.getColors(texture, quads[334], true)[1]
+    local color_info = ColorHelper.getColors(texture, quads[342], true)[1]
 
     local canvas = love.graphics.newCanvas(48, 48)
     canvas:renderTo(function() 
         love.graphics.setColor(1.0, 1.0, 1.0, 1.0)
 
-        love.graphics.draw(texture, quads[334], 0, 0)
-        love.graphics.draw(texture, quads[335], 16, 0)
-        love.graphics.draw(texture, quads[338], 32, 0)
+        love.graphics.draw(texture, quads[342], 0, 0)
+        love.graphics.draw(texture, quads[343], 16, 0)
+        love.graphics.draw(texture, quads[346], 32, 0)
 
-        love.graphics.draw(texture, quads[336], 0, 16)
-        love.graphics.draw(texture, quads[341], 32, 16)
+        love.graphics.draw(texture, quads[344], 0, 16)
+        love.graphics.draw(texture, quads[349], 32, 16)
 
-        love.graphics.draw(texture, quads[339], 0, 32)
-        love.graphics.draw(texture, quads[340], 16, 32)
-        love.graphics.draw(texture, quads[343], 32, 32)
+        love.graphics.draw(texture, quads[347], 0, 32)
+        love.graphics.draw(texture, quads[348], 16, 32)
+        love.graphics.draw(texture, quads[351], 32, 32)
 
         love.graphics.setColor(color_info.color)
         love.graphics.rectangle('fill', 16, 16, 16, 16)
@@ -95,29 +95,27 @@ M.generatePanelTexture = function(w, h)
     local texture = TextureCache:get('uf_interface')
     local quads = QuadCache:get('uf_interface')
 
-    local offset = 34 * 0 -- offset of 0, 1, 2 to change themes: gray, blue, brown
-
-    local color_info = ColorHelper.getColors(texture, quads[326 + offset], true)[1]
+    local color_info = ColorHelper.getColors(texture, quads[332], true)[1]
 
     local canvas = love.graphics.newCanvas(w, h)
     canvas:renderTo(function()
         love.graphics.setColor(1.0, 1.0, 1.0, 1.0)
 
-        love.graphics.draw(texture, quads[324 + offset], 0, 0)
-        love.graphics.draw(texture, quads[328 + offset], w - 16, 0)
-        love.graphics.draw(texture, quads[329 + offset], 0, h - 16)
-        love.graphics.draw(texture, quads[333 + offset], w - 16, h - 16)
+        love.graphics.draw(texture, quads[332], 0, 0)
+        love.graphics.draw(texture, quads[336], w - 16, 0)
+        love.graphics.draw(texture, quads[337], 0, h - 16)
+        love.graphics.draw(texture, quads[341], w - 16, h - 16)
 
         -- top & bottom rows
         for x = 16, w - 24, 8 do
-            love.graphics.draw(texture, quads[325 + offset], x, 0)
-            love.graphics.draw(texture, quads[330 + offset], x, h - 16)
+            love.graphics.draw(texture, quads[333], x, 0)
+            love.graphics.draw(texture, quads[338], x, h - 16)
         end
 
         -- middle
         for y = 16, h - 24, 8 do
-            love.graphics.draw(texture, quads[326 + offset], 0, y)
-            love.graphics.draw(texture, quads[331 + offset], w - 16, y)
+            love.graphics.draw(texture, quads[334], 0, y)
+            love.graphics.draw(texture, quads[339], w - 16, y)
         end
 
         love.graphics.setColor(unpack(color_info.color))
